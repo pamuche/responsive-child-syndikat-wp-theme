f9c7cd30acca676c2514205a8ac16e9aa3601952
Michael Prilop <Michael.Prilop@gmail.com>
2013-09-24 19:41:16 +0200

    added initial try for js-map (using jvectormap) to projekt liste

---
+++ js/map_config.js
@@ -0,0 +1,107 @@
+(function($) {
+	$(document)
+			.ready(
+					function() {
+
+						$.ajaxSetup({
+							cache : false
+						});
+
+						var pageLoaderID = 42;
+
+						$('#map')
+								.vectorMap(
+										{
+
+											map : 'de_merc_en',
+											backgroundColor: 'white',
+											regionStyle: {
+												  initial: {
+													    fill: 'white',
+													    "fill-opacity": 1,
+													    stroke: 'grey',
+													    "stroke-width": 1,
+													    "stroke-opacity": 1
+													  },
+													  hover: {
+													    "fill-opacity": 0.8
+													  },
+													  selected: {
+													    fill: 'yellow'
+													  },
+													  selectedHover: {
+													  }
+													},
+
+											onRegionClick : function(event,
+													code) {
+
+												var post_id;
+
+												switch (code) {
+
+												case "DE":
+
+													post_id = 30;
+
+													break;
+
+												case "US":
+
+													post_id = 54;
+
+													break;
+
+												case "FR":
+
+													post_id = 36;
+
+													break;
+
+												case "ES":
+
+													post_id = 56;
+
+													break;
+
+												default:
+
+													post_id = false;
+
+													break;
+
+												}
+
+												console.log('Country code: '
+														+ code + ' Post ID: '
+														+ post_id);
+
+												if (post_id !== false) {
+
+													$("#mapData").html(
+															"Loading data...");
+
+													$("#mapData")
+															.load(
+																	"http://tests.andremeyering.de/jvectormap/wp/?page_id="
+																			+ pageLoaderID,
+																	{
+																		id : post_id
+																	});
+
+												} else {
+
+													$("#mapData")
+															.html(
+																	"<p><strong>No Country data avaiable!<br></strong>Avaiable countries: Germany, USA, France and Spain.</p>");
+
+												}
+												return false;
+
+											}
+
+										});
+
+					});
+
+})(jQuery);
--- projekte-liste.php
+++ projekte-liste.php
@@ -11,9 +11,21 @@ if ( !defined('ABSPATH')) exit;
  * Template Name: Projekte Liste
  */
 
+
+wp_deregister_script( 'jquery' );
+wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', array(), null, false );
+
+wp_enqueue_style( 'jvectormap-style', get_stylesheet_directory_uri() . '/js/jquery-jvectormap.css', array(), '20120805', 'all' );
+wp_enqueue_script( 'jvectormap-script', get_stylesheet_directory_uri() . '/js/jquery-jvectormap.min.js', array( 'jquery' ), '20120805', true );
+wp_enqueue_script( 'jvectormap-map', get_stylesheet_directory_uri() . '/js/jquery-jvectormap-de-merc-en.js', array( 'jquery' ), '20120805', true );
+wp_enqueue_script( 'jvectormap-map_code', get_stylesheet_directory_uri() . '/js/map_config.js', array( 'jquery' ), '20120805', true );
+
+
+
 get_header(); ?>
 
-<div id="content-archive" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
+<!-- div id="content-archive" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>" -->
+<div id="content-archive" class="grid col-540">
 
 	<?php if (have_posts()) : ?>
         
@@ -21,7 +33,7 @@ get_header(); ?>
            
            
         <?php // ################################ BEGIN SYNDIKAT CUSTOM STUFF ########################?>
-                   
+
         <?php 
         $args = array( 'post_type' => 'projekte' );
         $loop_projekte = new WP_Query( $args );
@@ -88,6 +100,11 @@ get_header(); ?>
 	?>  
       
 </div><!-- end of #content-archive -->
-        
+        <div class='grid col-380 fit'>
+                   <div id="map">
+
+                   </div>
+        </div>
+
 <?php //get_sidebar(); ?>
 <?php get_footer(); ?>
