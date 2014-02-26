d9fd21ebfb788e64c6c8efc41c8aebcb629a0bd5
Michael Prilop <Michael.Prilop@gmail.com>
2014-02-26 19:17:36 +0100

    removed all meta information and navigation links not needed for projekte for #22

--- single-projekte.php
+++ single-projekte.php
@@ -35,8 +35,6 @@ get_header(); ?>
 				
 				<?php // ################################ BEGIN SYNDIKAT CUSTOM STUFF ########################?>
 				
-                <?php //get_template_part( 'post-meta' ); ?>
-
                 <div class="post-entry">             
                 
 				<div class="projektBlock alignleft">
@@ -71,45 +69,14 @@ get_header(); ?>
 				</div>
 	
 				<?php // ################################ END SYNDIKAT CUSTOM STUFF ########################?>
-
 			
-			
-				<?php the_content(__('Read more &#8250;', 'responsive')); ?>
-
-				<?php if ( get_the_author_meta('description') != '' ) : ?>
-
-				<div id="author-meta">
-					<?php if (function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email'), '80' ); }?>
-					<div class="about-author">
-						<?php _e('About','responsive'); ?>
-						<?php the_author_posts_link(); ?>
-					</div>
-					<p>
-						<?php the_author_meta('description') ?>
-					</p>
-				</div>
-				<!-- end of #author-meta -->
-
-				<?php endif; // no description, no author's meta ?>
-
-				<?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
+				<?php the_content(); ?>
 			</div>
 			<!-- end of .post-entry -->
-                
-                <div class="navigation">
-			        <div class="previous"><?php previous_post_link( '&#8249; %link' ); ?></div>
-                    <div class="next"><?php next_post_link( '%link &#8250;' ); ?></div>
-		        </div><!-- end of .navigation -->
-                
-                <?php get_template_part( 'post-data' ); ?>
-				               
+             
 				<?php responsive_entry_bottom(); ?>      
 			</div><!-- end of #post-<?php the_ID(); ?> -->       
-			<?php responsive_entry_after(); ?>            
-            
-			<?php responsive_comments_before(); ?>
-			<?php comments_template( '', true ); ?>
-			<?php responsive_comments_after(); ?>
+			<?php responsive_entry_after(); ?> 
             
         <?php 
 		endwhile; 
