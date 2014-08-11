dbb2d1ece91d5771037d8ab5b167b8b497b8fcfb
Michael Prilop <Michael.Prilop@gmail.com>
2014-08-11 16:32:28 +0200

    added description of failed projects

--- gescheiterten-liste.php
+++ gescheiterten-liste.php
@@ -18,7 +18,10 @@ $meta_query_for_projekte = array(
 		array('key' => 'ist_gescheitert', 'value' => '1', 'compare' => '==')
 );
 
-$description_of_type = __('');
+$description_of_type = __('Es gibt verschieden Gründe, warum Projekte nicht realisiert werden und sich Initiativen '
+		.'wieder auflösen. Beim Eilhardshof war es eine Insolvenz. Andere Gruppen haben sich aufgelöst, weil sie '
+		.'ihr Wunschobjekt nicht kaufen konnten, z B. weil sie überboten wurden. Die Auflistung ist unvollständig, '
+		.'da wir erst 2014 damit begonnen haben.');
 
 include 'projekte-initiativen-template.php'
 ?>
